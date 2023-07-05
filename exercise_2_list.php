0bd1a91f04e3a06941d9da07c816c932aefd7df4
Antonio Ladino <85692930+ladval@users.noreply.github.com>
2023-07-05 09:48:25 -0500

    Switch to methods approach for data structure sorting
    Switch to methods approach for data structure sorting

--- exercise_2_list.php
+++ exercise_2_list.php
@@ -7,35 +7,59 @@ require "ConsoleFormat.php";
  * @param array $data The array to be sorted.
  * @param array $sortKeys The keys used for sorting the array.
  */
-function sort_recursive(&$data, $sortKeys)
+function sortRecursiveByKeys(&$data, $keys)
 {
-    // Validate input parameters
-    if (!is_array($data)) {
-        throw new InvalidArgumentException('$data must be an array.');
-    }
-
-    if (!is_array($sortKeys) || empty($sortKeys)) {
-        throw new InvalidArgumentException('$sortKeys must be a non-empty array.');
-    }
-
-    if (count($sortKeys) === 0) {
-        return;
-    }
-
-    $currentKey = array_shift($sortKeys);
-
-    usort($data, function ($a, $b) use ($currentKey, $sortKeys) {
-        if (!array_key_exists($currentKey, $a) || !array_key_exists($currentKey, $b)) {
-            throw new InvalidArgumentException("Invalid sort key: $currentKey");
+    usort($data, function ($a, $b) use ($keys) {
+        foreach ($keys as $key) {
+            try {
+                $valueA = getValueByNestedKeys($a, $key);
+                $valueB = getValueByNestedKeys($b, $key);
+                if ($valueA != $valueB) {
+                    return $valueA <=> $valueB;
+                }
+            } catch (Exception $e) {
+                throw new Exception("An error occurred: " . $e->getMessage());
+            }
         }
-
-        if ($a[$currentKey] === $b[$currentKey]) {
-            sort_recursive($a, $sortKeys);
-            sort_recursive($b, $sortKeys);
-        }
-        return $a[$currentKey] <=> $b[$currentKey];
+        return 0;
     });
+
+    foreach ($data as &$item) {
+        foreach ($item as $key => &$value) {
+            if (is_array($value)) {
+                try {
+                    sortRecursiveByKeys($value, $keys);
+                } catch (Exception $e) {
+                    // Handle the exception here
+                    throw new Exception("An error occurred: " . $e->getMessage());
+                }
+            }
+        }
+    }
 }
+
+function getValueByNestedKeys($data, $keys)
+{
+    if (is_array($keys)) {
+        $currentValue = $data;
+        foreach ($keys as $key) {
+            try {
+                if (isset($currentValue[$key])) {
+                    $currentValue = $currentValue[$key];
+                } else {
+                    return null;
+                }
+            } catch (Exception $e) {
+                // Handle the exception here
+                throw new Exception("An error occurred: " . $e->getMessage());
+            }
+        }
+        return $currentValue;
+    } else {
+        return $data;
+    }
+}
+
 
 // Usage data structure:
 $filename = 'data/data_structure_lists.php';
@@ -51,7 +75,7 @@ if (!is_array($data)) {
 $sortKeys = ['last_name', 'account_id', 'booking_number'];
 
 // Sort the data recursively
-sort_recursive($data, $sortKeys);
+sortRecursiveByKeys($data, $sortKeys);
 
 // Print the sorted data
 ConsoleFormat::printNestedKeyValuePairs($data, $sortKeys);
